c9c1911e01f881a0ade237f2b2c3be1b956ccda2
leeruigan <leeruigan@gmail.com>
2021-09-04 15:45:38 +0800

    electron

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,88 +1,108 @@
 module.exports = {
-  base: '/zero/',
-  dest: 'dist',
-  title: 'Jason Lee',
-  description: '技术总结',
+  base: "/zero/",
+  dest: "dist",
+  title: "Jason Lee",
+  description: "技术总结",
   head: [
-    // ['link', { rel: 'icon', href: `/logo.png` }],
-    // ['link', { rel: 'manifest', href: '/manifest.json' }],
-    // ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    // ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    // ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    // ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
-    // ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    // ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-    // ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
+    ['meta', { name: 'keywords', content: 'blog'}],
+    ['meta', { name: 'theme-color', content: '#11a8cd'}], // 移动浏览器主题颜色
   ],
   markdown: {
     toc: {
-        includeLevel:[1, 2, 3, 4]
-    }
+      includeLevel: [1, 2, 3, 4],
+    },
   },
-  plugins: [
-    ['@dovyp/vuepress-plugin-clipboard-copy', true],
-  ],
+  theme: "vdoing",
+  plugins: [["@dovyp/vuepress-plugin-clipboard-copy", true]],
   serviceWorker: false,
   themeConfig: {
-    repo: 'LRG00/zero',
-    editLinks: true,
-    docsDir: 'docs',
-    editLinkText: '在 GitHub 上编辑此页',
-    lastUpdated: '上次更新',
+    repo: "LRG00/zero",
+    // editLinks: true, // 编辑链接
+    // docsDir: "docs", // 编辑的文件夹
+    sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+    // editLinkText: "在 GitHub 上编辑此页",
+    author: {
+      // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, href: String}
+      name: "jason lee", // 必需
+      href: "https://github.com/LRG00", // 可选的
+    },
+    lastUpdated: "上次更新",
+    footer: {
+      // 页脚信息
+      createYear: 2019, // 博客创建年份
+      copyrightInfo: "jason lee | MIT License", // 博客版权信息，支持a标签
+    },
     nav: [
+      { text: "首页", link: "/" },
       {
-        text: 'interview',
-        link: '/interview/shouxie/'
+        text: "interview",
+        link: "/pages/35a15c/",
+        items: [
+          { text: "css", link: "/pages/35a15c/" },
+          { text: "javascript", link: "/pages/1d1a2e/" },
+          { text: "错误监控", link: "/pages/ade6ea/" },
+          { text: "es6", link: "/pages/7d449f/" },
+          { text: "手写代码", link: "/pages/da0a0a/" },
+        ],
       },
       {
-        text: 'node',
-        link: '/node/new/'
+        text: "electron",
+        link: "/pages/54895f/",
+        items: [
+          {
+            text: "学习笔记",
+            link: "/pages/54895f/",
+          },
+        ],
+      },
+      {
+        text: "nodejs",
+        link: "/pages/4ff0e8/",
       },
     ],
-    sidebar: {
-      '/interview/': [
-        {
-          title: '手写JS',
-          collapsable: true,
-          children: [
-            ['shouxie/', '展开'],
-            // 'shouxie/bind',
-          ]
-        },
-        {
-          title: 'css',
-          collapsable: true,
-          children: [
-            ['css/', '展开'],
-            // 'shouxie/bind',
-          ]
-        },
-        {
-          title: 'js基础',
-          collapsable: true,
-          children: [
-            ['javascript/', '展开'],
-            // 'shouxie/bind',
-          ]
-        },
-        {
-          title: '错误监控',
-          collapsable: true,
-          children: [
-            ['error1/', '展开'],
-            // 'shouxie/bind',
-          ]
-        },
-        {
-          title: 'es6',
-          collapsable: true,
-          children: [
-            ['es6/', '展开'],
-            // 'shouxie/bind',
-          ]
-        },
-      ],
-      '/node/': []
-    }
-  }
-}
+    sidebar: { mode: "structuring", collapsable: false },
+    updateBar: {
+      // 最近更新栏
+      showToArticle: false, // 显示到文章页底部，默认true
+      // moreArticle: '/archives' // “更多文章”跳转的页面，默认'/archives'
+    },
+    // sidebar: {
+    //   '/interview/': [
+    //     {
+    //       title: '手写JS',
+    //       collapsable: true,
+    //     },
+    //     {
+    //       title: 'css',
+    //       collapsable: true,
+    //     },
+    //     {
+    //       title: 'js基础',
+    //       collapsable: true,
+    //       children: [
+    //         ['javascript/', '展开'],
+    //         // 'shouxie/bind',
+    //       ]
+    //     },
+    //     {
+    //       title: '错误监控',
+    //       collapsable: true,
+    //       children: [
+    //         ['error1/', '展开'],
+    //         // 'shouxie/bind',
+    //       ]
+    //     },
+    //     {
+    //       title: 'es6',
+    //       collapsable: true,
+    //       children: [
+    //         ['es6/', '展开'],
+    //         // 'shouxie/bind',
+    //       ]
+    //     },
+    //   ],
+    //   '/node/': []
+    // }
+  },
+};
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,11 @@
-# Hello VuePress1
+---
+home: true
+heroImage: https://vkceyugu.cdn.bspapp.com/VKCEYUGU-3609f3af-dcdf-46cf-9b42-d5c482759b0f/cd5a7e81-b17d-4149-a34a-bd8cd2e5dfe4.png
+tagline: 生活可能不像你想象的那么好，但是也不会像你想象的那么糟。
+actionLink: /pages/35a15c/
+# bannerBg: none # auto => 网格纹背景(有bodyBgImg时无背景)，默认 | none => 无 | '大图地址' | background: 自定义背景样式       提示：如发现文本颜色不适应你的背景时可以到palette.styl修改$bannerTextColor变量
+
+# 文章列表显示方式: detailed 默认，显示详细版文章列表（包括作者、分类、标签、摘要、分页等）| simple => 显示简约版文章列表（仅标题和日期）| none 不显示文章列表
+# postList: none
+---
+
