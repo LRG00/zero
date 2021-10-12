a281eaccaa12d40ca7e79737b5892a1b3814f900
leeruigan <leeruigan@gmail.com>
2021-10-12 15:58:18 +0800

    typescript

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -44,6 +44,7 @@ module.exports = {
             { text: "html&css", link: "/pages/6d546b/" },
             { text: "webpack", link: "/pages/fe0b24/" },
             { text: "vue3学习", link: "/pages/039d48/" },
+            { text: "typescript入门", link: "/pages/039d50/" },
             { text: "工程化实践", link: "/pages/039d49/" },
           ]}
         ],
